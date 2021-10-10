bd86c00438cd7dd7ce3e594d31cbe7935d498d82
Raj Sharma <yrsharma@icloud.com>
2021-10-10 16:05:29 +0100

    Pattern matching + proper erroring

--- src/main.py
+++ src/main.py
@@ -1,32 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import re
 
 import requests
 from gooey import Gooey
 
 
 def validate_webhook(url: str) -> bool:
+    regex = r'^https:\/\/discord(?:app)?\.com\/api\/webhooks\/'
+    if not re.match(regex, url):
+        return False
+
     resp = requests.get(url).json()
     return resp.get('code') is None
 
 
 def send_webhook(url: str, content: str = None) -> None:
     if not content:
-        err = 'Content must be specified'
-        raise TypeError(err)
+        print('Content must be specified')
+        exit()
 
     if not validate_webhook(url):
-        err = 'Webhook URL is invalid'
-        raise TypeError(err)
+        print('Webhook URL is invalid')
+        exit()
 
     data = {
         'content': content,
     }
     resp = requests.post(url, json=data)
     if not 200 <= resp.status_code < 300:
-        err = f'Request returned {resp.status_code}'
-        raise Exception(err)
+        print(f'Request returned {resp.status_code}')
+        exit()
+    print('Message sent successfully')
 
 
 @Gooey(program_name='Webhook.py')
@@ -37,8 +43,8 @@ def main() -> None:
     args = parser.parse_args()
 
     if not args.content:
-        err = 'Content must be specified'
-        raise TypeError(err)
+        print('Content must be specified')
+        exit()
 
     send_webhook(args.url, args.content)
 
