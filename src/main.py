466e929badffeecd09f926fb304b535918a986ec
Raj Sharma <yrsharma@icloud.com>
2021-10-10 13:26:25 +0100

    Fix line too long

--- src/main.py
+++ src/main.py
@@ -15,7 +15,9 @@ def main(args: argparse.Namespace) -> None:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A python GUI for sending Discord webhooks')
     parser.add_argument('-c', '--content', type=str, default=None, help='Message content (str)')
-    parser.add_argument('-f', '--file', type=argparse.FileType('rb'), default=None, help='File to attach (path)')
+    parser.add_argument(
+        '-f', '--file', type=argparse.FileType('rb'), default=None, help='File to attach (path)'
+    )
 
     _args = parser.parse_args()
     main(_args)
