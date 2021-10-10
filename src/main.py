38bed98b68691bc8940d56a4139b6a2ea523ddad
Raj Sharma <yrsharma@icloud.com>
2021-10-10 17:29:06 +0100

    Styling

--- src/main.py
+++ src/main.py
@@ -39,11 +39,9 @@ def send_webhook(
     if username:
         data['username'] = username
     if avatar_url:
-        if not re.match(
-                r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})',
-                avatar_url
-        ):
-            print(f'Invalid avatar URL')
+        regex = r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'
+        if not re.match(regex, avatar_url):
+            print('Invalid avatar URL')
             sys.exit()
         data['avatar_url'] = avatar_url
 
