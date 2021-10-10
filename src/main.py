cfbd512748b8e810d12480ee5d75bd6ef2b3a480
Raj Sharma <yrsharma@icloud.com>
2021-10-10 12:46:08 +0100

    Implement basic argparse

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 
-def main() -> None:
-    ...
+import argparse
+
+
+def main(args: argparse.Namespace) -> None:
+    if not (args.content or args.file):
+        err = 'One of content or file must be specified'
+        raise TypeError(err)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='A python GUI for sending Discord webhooks')
+    parser.add_argument('-c', '--content', type=str, default=None, help='Message content (str)')
+    parser.add_argument('-f', '--file', type=argparse.FileType('rb'), default=None, help='File to attach (path)')
+
+    _args = parser.parse_args()
+    main(_args)
